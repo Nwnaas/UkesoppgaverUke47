dd81ff1a229372af5c2dca51826267f0053f611d
“Nwnaas” <nwnaas@icloud.com>
2023-11-20 11:35:58 +0100

    test

---
+++ src/main/java/com/example/ukesoppgaveruke47/UkesoppgaverUke47Application.java
@@ -0,0 +1,13 @@
+package com.example.ukesoppgaveruke47;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UkesoppgaverUke47Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UkesoppgaverUke47Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ukesoppgaveruke47/UkesoppgaverUke47ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ukesoppgaveruke47;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UkesoppgaverUke47ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
